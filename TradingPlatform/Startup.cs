1144f38db611eb4e73c9a78a40dacdca842830b6
Andrey Minin <mandrey0501@gmail.com>
2020-08-25 02:56:28 +0500

    Добавлен Swagger

--- TradingPlatform/Startup.cs
+++ TradingPlatform/Startup.cs
@@ -3,21 +3,34 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace TradingPlatform
 {
     public class Startup
     {
+        private const string apiVersion = "v1";
+        private const string systemName = "Trading Platform API";
+        
+        public IConfiguration Configuration { get; }
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
-
-        public IConfiguration Configuration { get; }
-
+        
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc(apiVersion, new OpenApiInfo
+                {
+                    Version = apiVersion,
+                    Title = systemName,
+                    Description = @"Реализация API системы ""Торговая площадка продажи игровых ключей"""
+                });
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -26,14 +39,18 @@ namespace TradingPlatform
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            
             app.UseHttpsRedirection();
-
             app.UseRouting();
-
             app.UseAuthorization();
-
             app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+            
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint($"/swagger/{apiVersion}/swagger.json", systemName);
+                c.RoutePrefix = string.Empty;
+            });
         }
     }
 }
